1fd59ee6eefb9a2c42b245bdca2ccb0abd13b49b
Abdellatif Nahal <abdnahal@c3r9p7.1337.ma>
2025-11-03 09:45:14 +0100

    V3

--- ft_printf.c
+++ ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: abdnahal <abdnahal@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/29 17:01:04 by abdnahal          #+#    #+#             */
-/*   Updated: 2025/11/02 09:16:29 by abdnahal         ###   ########.fr       */
+/*   Updated: 2025/11/03 09:42:24 by abdnahal         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -76,8 +76,127 @@ int	ft_printf(const char *s, ...)
 	return (count);
 }
 
-// int	main(void)
+// int main()
 // {
-// 	printf("%d\n", printf(" %i ", -9));
-// 	printf("%d\n", ft_printf(" %i ", -9));
+
+// 	int ret1, ret2;
+
+// 	printf("===== BASIC TESTS =====\n");
+
+// 	ret1 = printf("1: %d\n", 0);
+// 	ret2 = ft_printf("1: %d\n", 0);
+// 	printf("printf ret: %d | ft_printf ret: %d\n\n", ret1, ret2);
+
+// 	ret1 = printf("2: %d\n", -42);
+// 	ret2 = ft_printf("2: %d\n", -42);
+// 	printf("printf ret: %d | ft_printf ret: %d\n\n", ret1, ret2);
+
+// 	ret1 = printf("3: %i\n", 2147483647);
+// 	ret2 = ft_printf("3: %i\n", 2147483647);
+// 	printf("printf ret: %d | ft_printf ret: %d\n\n", ret1, ret2);
+
+// 	ret1 = printf("4: %d\n", -2147483648);
+// 	ret2 = ft_printf("4: %d\n", -2147483648);
+// 	printf("printf ret: %d | ft_printf ret: %d\n\n", ret1, ret2);
+
+// 	ret1 = printf("5: %u\n", 4294967295u);
+// 	ret2 = ft_printf("5: %u\n", 4294967295u);
+// 	printf("printf ret: %d | ft_printf ret: %d\n\n", ret1, ret2);
+
+// 	printf("===== CHAR AND STRING =====\n");
+
+// 	ret1 = printf("6: %c %c %c\n", 'A', 0, 'Z');
+// 	ret2 = ft_printf("6: %c %c %c\n", 'A', 0, 'Z');
+// 	printf("printf ret: %d | ft_printf ret: %d\n\n", ret1, ret2);
+
+// 	ret1 = printf("7: %s\n", "Hello");
+// 	ret2 = ft_printf("7: %s\n", "Hello");
+// 	printf("printf ret: %d | ft_printf ret: %d\n\n", ret1, ret2);
+
+// 	ret1 = printf("8: %s\n", "");
+// 	ret2 = ft_printf("8: %s\n", "");
+// 	printf("printf ret: %d | ft_printf ret: %d\n\n", ret1, ret2);
+
+// 	ret1 = printf("9: %s\n", NULL);
+// 	ret2 = ft_printf("9: %s\n", NULL);
+// 	printf("printf ret: %d | ft_printf ret: %d\n\n", ret1, ret2);
+
+// 	printf("===== HEXADECIMAL =====\n");
+
+// 	ret1 = printf("10: %x\n", 255);
+// 	ret2 = ft_printf("10: %x\n", 255);
+// 	printf("printf ret: %d | ft_printf ret: %d\n\n", ret1, ret2);
+
+// 	ret1 = printf("11: %X\n", 255);
+// 	ret2 = ft_printf("11: %X\n", 255);
+// 	printf("printf ret: %d | ft_printf ret: %d\n\n", ret1, ret2);
+
+// 	printf("===== POINTERS =====\n");
+
+// 	ret1 = printf("12: %p\n", (void *)0);
+// 	ret2 = ft_printf("12: %p\n", (void *)0);
+// 	printf("printf ret: %d | ft_printf ret: %d\n\n", ret1, ret2);
+
+// 	ret1 = printf("13: %p\n", (void *)1234);
+// 	ret2 = ft_printf("13: %p\n", (void *)1234);
+// 	printf("printf ret: %d | ft_printf ret: %d\n\n", ret1, ret2);
+
+// 	ret1 = printf("14: %p %p\n", (void *)LONG_MIN, (void *)LONG_MAX);
+// 	ret2 = ft_printf("14: %p %p\n", (void *)LONG_MIN, (void *)LONG_MAX);
+// 	printf("printf ret: %d | ft_printf ret: %d\n\n", ret1, ret2);
+
+// 	printf("===== PERCENT SIGN =====\n");
+
+// 	ret1 = printf("15: %%\n");
+// 	ret2 = ft_printf("15: %%\n");
+// 	printf("printf ret: %d | ft_printf ret: %d\n\n", ret1, ret2);
+
+// 	ret1 = printf("16: %%%% DONE\n");
+// 	ret2 = ft_printf("16: %%%% DONE\n");
+// 	printf("printf ret: %d | ft_printf ret: %d\n\n", ret1, ret2);
+
+// 	printf("===== MIXED & MULTIPLE =====\n");
+
+// 	ret1 = printf("17: Hello %s, your score is %d/%u!\n", "Bob", 42, 100);
+// 	ret2 = ft_printf("17: Hello %s, your score is %d/%u!\n", "Bob", 42, 100);
+// 	printf("printf ret: %d | ft_printf ret: %d\n\n", ret1, ret2);
+
+// 	ret1 = printf("18: Hex: %x, Unsigned: %u, Dec: %d\n", 255, 255, 255);
+// 	ret2 = ft_printf("18: Hex: %x, Unsigned: %u, Dec: %d\n", 255, 255, 255);
+// 	printf("printf ret: %d | ft_printf ret: %d\n\n", ret1, ret2);
+
+// 	ret1 = printf("19: %d %s %c %p %%\n", 42, "test", '!', (void *)42);
+// 	ret2 = ft_printf("19: %d %s %c %p %%\n", 42, "test", '!', (void *)42);
+// 	printf("printf ret: %d | ft_printf ret: %d\n\n", ret1, ret2);
+
+// 	printf("===== STRESS & EDGE =====\n");
+
+// 	ret1 = printf("20: %d%d%d%d%d%d%d%d%d%d\n", 1,2,3,4,5,6,7,8,9,10);
+// 	ret2 = ft_printf("20: %d%d%d%d%d%d%d%d%d%d\n", 1,2,3,4,5,6,7,8,9,10);
+// 	printf("printf ret: %d | ft_printf ret: %d\n\n", ret1, ret2);
+
+// 	ret1 = printf("21: %s\n", "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa");
+// 	ret2 = ft_printf("21: %s\n", "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa");
+// 	printf("printf ret: %d | ft_printf ret: %d\n\n", ret1, ret2);
+
+// 	ret1 = printf("22: %d %i %u %x %X\n", -1, -42, 4294967295u, -1, -42);
+// 	ret2 = ft_printf("22: %d %i %u %x %X\n", -1, -42, 4294967295u, -1, -42);
+// 	printf("printf ret: %d | ft_printf ret: %d\n\n", ret1, ret2);
+
+// 	printf("===== INVALID & WEIRD =====\n");
+
+// 	ret1 = printf("23: %q\n"); // invalid specifier
+// 	ret2 = ft_printf("23: %q\n");
+// 	printf("printf ret: %d | ft_printf ret: %d\n\n", ret1, ret2);
+
+// 	ret1 = printf("24: %s%s%s\n", NULL, NULL, "end");
+// 	ret2 = ft_printf("24: %s%s%s\n", NULL, NULL, "end");
+// 	printf("printf ret: %d | ft_printf ret: %d\n\n", ret1, ret2);
+
+// 	ret1 = printf("25: %p %p %p\n", NULL, NULL, NULL);
+// 	ret2 = ft_printf("25: %p %p %p\n", NULL, NULL, NULL);
+// 	printf("printf ret: %d | ft_printf ret: %d\n\n", ret1, ret2);
+
+// 	printf("===== DONE =====\n");
+// 	return (0);
 // }
